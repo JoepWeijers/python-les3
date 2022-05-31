3056910e202a9f2f95ab2bb4dded0bd74594cbdd
Joep Weijers <j.weijers@topdesk.com>
2022-05-31 20:15:13 +0200

    Wijzig programmeerclub in fizzbuzz

--- programmeerclub.py
+++ programmeerclub.py
@@ -1,12 +1,12 @@
 for getal in range(100):
     if getal % 3 == 0 and getal % 5 == 0:
-        print("programmeerclub")
+        print("fizzbuzz")
         continue
     elif getal % 3 == 0:
-        print("programmeer")
+        print("fizz")
         continue
     elif getal % 5 == 0:
-        print("club")
+        print("buzz")
         continue
     
     print(getal)
