a2787c5666410b436cecd4ad24d50cb9814bad06
Joep Weijers <j.weijers@topdesk.com>
2022-05-31 20:13:15 +0200

    Eerste commit voegt programmeerclub.py toe

---
+++ programmeerclub.py
@@ -0,0 +1,13 @@
+for getal in range(100):
+    if getal % 3 == 0 and getal % 5 == 0:
+        print("programmeerclub")
+        continue
+    elif getal % 3 == 0:
+        print("programmeer")
+        continue
+    elif getal % 5 == 0:
+        print("club")
+        continue
+    
+    print(getal)
+	
